195f8b949c6bfaa352f34336dff2edea9ec8b0c0
TunaCuma <tunacuma2003@gmail.com>
2024-12-15 12:31:13 +0300

    cleanup

--- src/app/app/(debug)/debug/page.tsx
+++ src/app/app/(debug)/debug/page.tsx
@@ -2,9 +2,6 @@
 import FootballPasses, {
   type PassData,
 } from "@/components/graphs/SoccerPitchArrows";
-import { useEffect, useState } from "react";
-import { fetchAndStyleHtml } from "@/lib/utils";
-import Buttons from "./Buttons";
 
 const sampleData: PassData[] = [
   {
@@ -24,56 +21,11 @@ const sampleData: PassData[] = [
 ];
 
 function App() {
-  const [htmlContent, setHtmlContent] = useState("");
-  const [htmlContent2, setHtmlContent2] = useState("");
-
-  useEffect(() => {
-    const fetchContent = async () => {
-      const styledHtml = await fetchAndStyleHtml("/api/python/passes");
-
-      if (styledHtml) {
-        console.log("styledHtml:", styledHtml);
-        setHtmlContent(styledHtml);
-      }
-    };
-
-    const fetchContent2 = async () => {
-      const styledHtml = await fetchAndStyleHtml("/api/python/dominations");
-
-      if (styledHtml) {
-        console.log("styledHtml2:", styledHtml);
-        setHtmlContent2(styledHtml);
-      }
-    };
-
-    const runFetches = async () => {
-      await fetchContent();
-      await fetchContent2();
-    };
-
-    runFetches();
-  }, []);
-
   return (
     <>
       <div className="mx-auto grid w-full grid-cols-2 items-center justify-center gap-2">
         <div className="flex flex-col items-center gap-4">
-          <div
-            className="relative h-[875px] w-[990px] transform-gpu overflow-hidden rounded-lg"
-            dangerouslySetInnerHTML={{ __html: htmlContent }}
-          />
-          <Buttons />
-        </div>
-        <div className="flex flex-col items-center gap-4">
-          <div
-            className="relative h-[475px] w-[700px] transform-gpu overflow-hidden rounded-lg"
-            dangerouslySetInnerHTML={{ __html: htmlContent2 }}
-          />
-          <Buttons />
-        </div>
-        <div className="flex flex-col items-center gap-4">
           <FootballPasses data={sampleData} size={700} grassPattern="striped" />
-          <Buttons />
         </div>
       </div>
     </>
